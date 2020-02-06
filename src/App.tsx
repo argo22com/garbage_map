46b3c5d88a9a20a0b2295582e5d908b3d2865b01
Ján Gál <jan.gal@argo22.com>
2020-02-06 22:42:50 +0100

    intelij: added Prettier watcher for TS and TS JSX files

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import { Map } from './components/Map';
-import './App.css';
-import 'leaflet/dist/leaflet.css';
+import React from "react";
+import { Map } from "./components/Map";
+import "./App.css";
+import "leaflet/dist/leaflet.css";
 
 const App = () => {
   return (
-    <div style={{width: "100vw", height: "100vh"}}>
+    <div style={{ width: "100vw", height: "100vh" }}>
       <Map />
     </div>
   );
