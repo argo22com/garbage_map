b58f4a973bdb6cc23ca0ed8665d37a079e1943c9
Honza <honza.gal@gmail.com>
2020-02-06 22:16:25 +0100

    init leaflet map (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Map } from './components/Map';
 import './App.css';
+import 'leaflet/dist/leaflet.css';
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{width: "100vw", height: "100vh"}}>
+      <Map />
     </div>
   );
 }
