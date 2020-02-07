65517784fcaacec5b4d88f881f2498324b88a71b
Honza <honza.gal@gmail.com>
2020-02-07 23:52:07 +0100

    Use spots from the datasource to render markers (#16)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-import React from "react";
 import { Map } from "components/Map";
+import { getSpots, Spot } from "datasource";
+import "leaflet/dist/leaflet.css";
+import React, { useEffect, useState } from "react";
+import { Marker } from "react-leaflet";
 
 const App = () => {
+  const [spots, setSpots] = useState<Spot[]>([]);
+
+  useEffect(() => {
+    getSpots().then(spots => {
+      setSpots(spots);
+    });
+  }, []);
+
   return (
-    <div className="w-screen h-screen">
-      <Map />
+    <div className="w-screen h-screen￿">
+      <Map>
+        {spots.map(item => (
+          <Marker
+            key={item.uid}
+            position={[item.location.latitude, item.location.longitude]}
+          />
+        ))}
+      </Map>
     </div>
   );
 };
