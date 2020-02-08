c4acc80aba528c7cf331208ffe7dae63add13fd7
Ján Gál <jan.gal@argo22.com>
2020-02-08 01:17:43 +0100

    init cluster support

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import "leaflet/dist/leaflet.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import * as serviceWorker from "serviceWorker";
+require("react-leaflet-markercluster/dist/styles.min.css");
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
