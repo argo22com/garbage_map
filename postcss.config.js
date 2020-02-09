15b75b24bb6a285c8851b1cd39ff4cc68f8211c6
Ján Gál <jan.gal@argo22.com>
2020-02-09 16:30:22 +0100

    added purgeCSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,25 @@
 const tailwindcss = require("tailwindcss");
+// postcss.config.js
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  // Specify the paths to all of the template files in your project
+  content: [
+    "./src/**/*.tsx"
+    // etc.
+  ],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+
+  // ignore Leaflet CSS
+  whitelistPatterns: [/^leaflet?/, /^marker?/, /^lvml?/],
+  whitelistPatternsChildren: [/^leaflet?/, /^marker?/, /^lvml?/]
+});
 
 module.exports = {
-  plugins: [tailwindcss("./tailwind.js"), require("autoprefixer")]
+  plugins: [
+    tailwindcss({
+      config: "./tailwind.js"
+    }),
+    purgecss
+  ]
 };
