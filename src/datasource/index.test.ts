b788567a118a38d8eccf3d664662c6b2d4210c55
Martin Janeček <admin@ikw.cz>
2020-02-21 22:13:58 +0100

    Allow filtering by container type (#29)

--- src/datasource/index.test.ts
+++ src/datasource/index.test.ts
@@ -1,7 +1,45 @@
-import { getSpots } from "datasource/index";
+import { ContainerType, getSpots } from "datasource/index";
 
 test("getSpots returns non-empty list", () => {
   getSpots().then(data => {
+    expect(data.length).toBeGreaterThan(0);
+  });
+});
+
+test("getSpots correctly filters by single containerTypes", () => {
+  getSpots({
+    containerTypes: [ContainerType.metals]
+  }).then(data => {
+    data.forEach(spot => {
+      expect(
+        spot.containers.filter(
+          // every spot has at least one metal container
+          container => container.type === ContainerType.metals
+        ).length
+      ).toBeGreaterThan(0);
+    });
+  });
+});
+
+test("getSpots correctly filters by multiple containerTypes", () => {
+  getSpots({
+    containerTypes: [ContainerType.metals, ContainerType.clearGlass]
+  }).then(data => {
+    data.forEach(spot => {
+      expect(
+        spot.containers.filter(
+          // every spot has at least one metal container
+          container => container.type === ContainerType.metals
+        ).length
+      ).toBeGreaterThan(0);
+      expect(
+        spot.containers.filter(
+          // every spot has at least one clearGlass container
+          container => container.type === ContainerType.clearGlass
+        ).length
+      ).toBeGreaterThan(0);
+    });
+    // and at least one matched
     expect(data.length).toBeGreaterThan(0);
   });
 });
