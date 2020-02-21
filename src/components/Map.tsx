0116ead917cdf19690b5f10033ddb89bfc67c843
Ján Gál <jan.gal@argo22.com>
2020-02-21 21:53:58 +0100

    add map provider toggle

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import { MapProviderToggle } from "components/MapProviderToggle";
+import {
+  MAP_PROVIDER_DEFAULT,
+  MAP_PROVIDER_SATELLITE,
+  MapProvider
+} from "consts";
+import React, { useState } from "react";
 import { Map as LeafletMap, TileLayer } from "react-leaflet";
 
 export const Map: React.FC = ({ children }) => {
+  const [mapProvider, setMapProvider] = useState<MapProvider>(
+    MAP_PROVIDER_DEFAULT
+  );
+
   /*
    * default icon is broken, this is temp (copy-paste) solution
    * source: https://github.com/PaulLeCam/react-leaflet/issues/453#issuecomment-410450387
@@ -19,14 +29,35 @@ export const Map: React.FC = ({ children }) => {
     });
   }, []);
 
+  const handleChangeProvider = (provider: MapProvider) =>
+    setMapProvider(provider);
+
   return (
-    <LeafletMap center={position} zoom={13} className="flex-grow" maxZoom={19}>
-      <TileLayer
-        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      {children}
-    </LeafletMap>
+    <div>
+      <LeafletMap
+        center={position}
+        zoom={13}
+        className="w-screen h-screen"
+        maxZoom={19}
+      >
+        <TileLayer
+          attribution={mapProvider.options.attribution}
+          url={mapProvider.url}
+        />
+        {children}
+      </LeafletMap>
+      <div
+        className="absolute bottom-0 left-0 m-2"
+        // TODO: replace via className
+        style={{ zIndex: 400 }}
+      >
+        <MapProviderToggle
+          providerDefault={MAP_PROVIDER_DEFAULT}
+          providerSatellite={MAP_PROVIDER_SATELLITE}
+          onChange={handleChangeProvider}
+        />
+      </div>
+    </div>
   );
 };
 
