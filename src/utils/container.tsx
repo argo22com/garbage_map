221334a2fb760b238cfa4dd2cc6258f67ac0fd9c
Ján Gál <jan.gal@argo22.com>
2020-02-21 21:37:24 +0100

    refactored getContainerColor method

--- src/utils/container.tsx
+++ src/utils/container.tsx
@@ -1,28 +1,19 @@
 import { ContainerType } from "datasource";
 
-export function getContainerColor(type: ContainerType): string {
-  if (type === ContainerType.paper) {
-    return "text-container-paper";
-  }
-  if (type === ContainerType.plastic) {
-    return "text-container-plastics";
-  }
-  if (type === ContainerType.clearGlass) {
-    return "text-container-glass_white";
-  }
-  if (type === ContainerType.mixedGlass) {
-    return "text-container-glass_mixed";
-  }
-  if (type === ContainerType.textile) {
-    return "text-container-textile";
-  }
-  if (type === ContainerType.oilsAndFats) {
-    return "text-container-oils_edible";
-  }
-  if (type === ContainerType.metals) {
-    return "text-container-metals";
-  }
+const containerColors: { [key in ContainerType]: string } = {
+  [ContainerType.paper]: "text-container-paper",
+  [ContainerType.plastic]: "text-container-plastics",
+  [ContainerType.clearGlass]: "text-container-glass_white",
+  [ContainerType.mixedGlass]: "text-container-glass_mixed",
+  [ContainerType.textile]: "text-container-textile",
+  [ContainerType.oilsAndFats]: "text-container-oils_edible",
+  [ContainerType.metals]: "text-container-metals"
+};
 
-  console.warn("Unknown trash type:", type);
-  return "text-black";
+export function getContainerColor(type: ContainerType): string {
+  if (!containerColors[type]) {
+    console.warn("undefined container type:", type);
+    return "text-black";
+  }
+  return containerColors[type];
 }
