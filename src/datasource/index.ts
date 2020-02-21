449b99671ed96cabfa0749a537627e5829f8b8e3
Martin Janeček <admin@ikw.cz>
2020-02-21 21:41:10 +0100

    Allow filtering by container type

--- src/datasource/index.ts
+++ src/datasource/index.ts
@@ -29,9 +29,30 @@ export type Spot = {
   containers: Container[];
 };
 
-export function getSpots(): Promise<Spot[]> {
+export type Filters = {
+  /** List of container types user is interested in. Spot must have all specified container types to be included.
+   * To disable this filter, set it to undefined (don't specify it), as empty array will return zero spots. */
+  containerTypes?: ContainerType[];
+};
+
+function applyFilters(filters: Filters, spots: Spot[]) {
+  const containerTypes = filters.containerTypes;
+  if (containerTypes) {
+    spots = spots.filter(spot =>
+      containerTypes.every(type =>
+        spot.containers.find(container => container.type === type)
+      )
+    );
+  }
+  return spots;
+}
+
+export function getSpots(filters: Filters = {}): Promise<Spot[]> {
   return new Promise(async (resolve, reject) => {
     // TODO: implement datasource selection once we need it
-    resolve(await getMappedData());
+    let spots = await getMappedData();
+    spots = applyFilters(filters, spots);
+
+    resolve(spots);
   });
 }
