5ef9f7d30a1871284ff00440bf4c39e3df13a02f
Ján Gál <jan.gal@argo22.com>
2020-03-28 13:52:19 +0100

    Add custom Leaflet Cluster component

---
+++ src/components/LeafletCluster.tsx
@@ -0,0 +1,29 @@
+import L, { Marker } from "leaflet";
+import "leaflet.markercluster/dist/leaflet.markercluster";
+import "leaflet.markercluster/dist/MarkerCluster.css";
+import "leaflet.markercluster/dist/MarkerCluster.Default.css";
+import React, { useEffect } from "react";
+import { useLeaflet } from "react-leaflet";
+
+const markerClusterGroup = L.markerClusterGroup({
+  disableClusteringAtZoom: 17,
+  spiderfyOnMaxZoom: false
+});
+
+type Props = {
+  markers: Marker[];
+};
+
+export const LeafletCluster: React.FC<Props> = ({ markers }) => {
+  const { map } = useLeaflet();
+
+  useEffect(() => {
+    markerClusterGroup.clearLayers();
+
+    markers.map(marker => marker.addTo(markerClusterGroup));
+
+    map?.addLayer(markerClusterGroup);
+  }, [markers, map]);
+
+  return null;
+};
