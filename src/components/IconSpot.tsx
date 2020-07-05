561bf69384e624d34355fa1fb53b76083e0649f1
Ján Gál <jan.gal@argo22.com>
2020-07-05 17:46:20 +0200

    rewrite container utils

--- src/components/IconSpot.tsx
+++ src/components/IconSpot.tsx
@@ -2,7 +2,7 @@ import classNames from "classnames";
 import { Icon } from "components/Icon";
 import { Container } from "datasource";
 import React from "react";
-import { getContainerColor } from "utils/container";
+import { containerTypes } from "utils/container";
 
 type Props = {
   containers: Container[];
@@ -15,10 +15,11 @@ export const IconSpot: React.FC<Props> = ({ containers }) => {
         <div
           className={index > 0 ? "-ml-1" : ""}
           key={`spot_marker-trash-${container.uid}`}
+          title={containerTypes[container.type].name}
         >
           <Icon
             type={"trash"}
-            className={classNames("w-2", getContainerColor(container.type))}
+            className={classNames("w-2", containerTypes[container.type].color)}
           />
         </div>
       ))}
