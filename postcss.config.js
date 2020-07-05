d51ad77d8fc4610f89afddbb1193e479a915f37c
Ján Gál <jan.gal@argo22.com>
2020-07-05 19:03:13 +0200

    add containers.ts file to the purgeCSS config

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,8 @@ const tailwindcss = require("tailwindcss");
 const purgecss = require("@fullhuman/postcss-purgecss")({
   // Specify the paths to all of the template files in your project
   content: [
-    "./src/**/*.tsx"
+    "./src/**/*.tsx",
+    "./src/utils/container.ts"
     // etc.
   ],
 
