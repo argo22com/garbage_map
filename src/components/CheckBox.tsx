c80106ffad4cd606b2bba2787e519af98f0fae14
Ján Gál <jan.gal@argo22.com>
2020-07-05 17:53:29 +0200

    add CheckBox component

---
+++ src/components/CheckBox.tsx
@@ -0,0 +1,33 @@
+import classNames from "classnames";
+import React, { ChangeEvent, ReactNode, ReactText, useCallback } from "react";
+
+type Props<T> = {
+  value: T;
+  checked: boolean;
+  onChange: (checked: boolean, value: T) => void;
+
+  className?: string;
+  label?: ReactNode;
+};
+
+export const CheckBox = <T extends ReactText>(_: Props<T>) => {
+  const handleChange = useCallback(
+    (event: ChangeEvent<HTMLInputElement>) =>
+      _.onChange(event.currentTarget.checked, _.value),
+    [_]
+  );
+
+  return (
+    <label
+      className={classNames("flex items-center cursor-pointer", _.className)}
+    >
+      <input
+        type="checkbox"
+        onChange={handleChange}
+        checked={_.checked}
+        value={_.value}
+      />
+      <div className={_.label ? "ml-2" : ""}>{_.label}</div>
+    </label>
+  );
+};
