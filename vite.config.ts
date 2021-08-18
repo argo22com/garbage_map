55d113376e672010b1a6dc295d5836ac23e3bcc8
Ján Gál <jan.gal@argo22.com>
2021-08-18 16:18:34 +0200

    add vite-react-jsx plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import reactRefresh from "@vitejs/plugin-react-refresh";
 import tsResolver from "vite-tsconfig-paths";
 import svgr from "vite-plugin-svgr";
 import eslintPlugin from "vite-plugin-eslint";
+import reactJsx from "vite-react-jsx";
 
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [
+    reactJsx(),
     eslintPlugin(),
     tsResolver(), // https://github.com/aleclarson/vite-tsconfig-paths
     reactRefresh(),
