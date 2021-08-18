e7f43be02e73a91a7d7bd020cdb1221da3322743
Ján Gál <jan.gal@argo22.com>
2021-08-18 16:48:30 +0200

    allow babel.config.js for the jest only

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,16 @@
-module.exports = {
-    presets: [
-        ['@babel/preset-env', {targets: {node: 'current'}}],
-        '@babel/preset-typescript',
-    ],
+/**
+ * Required by jest.
+ * This config can break build, use it for the jest tests only
+ */
+module.exports = (api) => {
+    const isTest = api.env('test');
+
+    if (!isTest) return {};
+
+    return {
+        presets: [
+            ['@babel/preset-env', {targets: {node: 'current'}}],
+            '@babel/preset-typescript',
+        ]
+    }
 };
